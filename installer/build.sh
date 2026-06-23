2338cd86acd46e0b4bd8f7a00ac20cdca9ba6014
Antheas Kapenekakis <git@antheas.dev>
2026-06-23 15:49:15 +0200

    fix typo for filelight

--- installer/build.sh
+++ installer/build.sh
@@ -137,7 +137,7 @@ flatpak install --system -y --noninteractive flathub \
     "app/org.kde.gwenview/${flatpak_arch}/stable" \
     "app/org.kde.kate/${flatpak_arch}/stable" \
     "app/org.kde.ark/${flatpak_arch}/stable" \
-    "apps/org.kde.filelight/${flatpak_arch}/stable"
+    "app/org.kde.filelight/${flatpak_arch}/stable"
 flatpak update --system --appstream -y --noninteractive
 
 rm -rf /var/lib/flatpak-installer
